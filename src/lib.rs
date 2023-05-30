463c310f8c32dd3094ca3964fdc002be48ddecf5
Patrick Oppel <patrick.oppel94@gmail.com>
2023-05-30 15:26:31 +1000

    change of format and remove duplicate struct + enum definitions to make it work with CLI

--- src/lib.rs
+++ src/lib.rs
@@ -14,30 +14,63 @@ use cargo_metadata::*;
 use std::fs;
 use std::io::Write;
 use serde_json::{json, to_string_pretty};
+use std::io::Read;
 
 #[proc_macro]
 pub fn print_json(input: TokenStream) -> TokenStream {
 
-    let parser = Punctuated::<TypePath, Token![,]>::parse_terminated;
-    let mut args = parser.parse(input).unwrap();
+    // let parser = Punctuated::<TokenStream2, Token![;]>::parse_terminated;
+    // let mut args = parser.parse2(input.into()).unwrap();
 
-    let mut vec = args.into_iter().collect::<Vec<_>>();
-    
-    let mut command = vec.remove(0).path.segments.first().unwrap().ident.to_string();
-    
-    let (name, typ): (Vec<_>,Vec<_>) = vec.into_iter().enumerate().partition(|(i, _)| i % 2 == 0);
-    
-    let mut json = String::new();
-    for ((_,n),(_,t)) in name.into_iter().zip(typ.into_iter()) {
-        json.push_str(&format!("\t{}: ", n.path.segments.first().unwrap().ident.to_string()));
-        json.push_str(&format!("{}\n", arguments(t)));        
+    // let mut vec = args.into_iter().collect::<Vec<_>>();
+    // let 
+    let input = input.to_string();
+    let input = input.replace(",,", ",");
+    let mut vec_str: Vec<String> = input.split(";").map(|s| s.to_string()).collect();
+    vec_str.pop();
+    for v in &vec_str {
+        println!("{}", v);
+    }
+
+    let mut enumstruct = String::new();
+    let mut commands = String::new();
+    for mut v in vec_str {  
+        println!("v: {}", v);      
+        let mut input = TokenStream::from_str(&v).unwrap();
+        let parser = Punctuated::<TypePath, Token![,]>::parse_terminated;
+        let mut args = parser.parse(input).unwrap();
+
+        let mut vec = args.into_iter().collect::<Vec<_>>();      
+        
+        let mut command = "# ".to_string();
+        println!("{:?}", command);
+        
+        command.push_str(&vec.remove(0).path.segments.first().unwrap().ident.to_string());
+        
+        let (name, typ): (Vec<_>,Vec<_>) = vec.into_iter().enumerate().partition(|(i, _)| i % 2 == 0);
+        
+        let mut json = String::new();        
+        for ((_,n),(_,t)) in name.into_iter().zip(typ.into_iter()) {
+            json.push_str(&format!("\t{}: ", n.path.segments.first().unwrap().ident.to_string()));
+            let (s,t) = arguments(t);
+            json.push_str(&format!("{},\n", s));
+            if t.is_some() {
+                enumstruct.push_str(&format!("{}",t.unwrap()));
+            }        
+        }
+        
+        if !json.is_empty() {
+            command.push_str(&format!(" {{\n{}}}\n", json));
+        } else {
+            command.push_str(": {}\n");
+        }
+
+        commands.push_str(&format!("{}\n", command));
     }
     
-    if !json.is_empty() {
-        command.push_str(&format!(": {{\n{}}}\n", json));
-    } else {
-        command.push_str(": {}\n");
-    }
+    commands.push_str(&format!("{}\n", remove_duplicates(&enumstruct)));
+    println!("{}", commands);
+    // println!("{}", enumstruct);
 
     let mut file = std::fs::OpenOptions::new()
         .create(true)
@@ -45,89 +78,123 @@ pub fn print_json(input: TokenStream) -> TokenStream {
         .open("commands.json")
         .expect("Failed to open file");
 
-    write!(file, "{}", command).expect("Failed to write to file");
+    write!(file, "{}", commands).expect("Failed to write to file");
+    // write!(file, "{}", enumstruct).expect("Failed to write to file");
 
     TokenStream::new()
 }
 
-// #[proc_macro]
-fn arguments(path: TypePath) -> String {
-    let depth: usize = 0;
-    if let Some(t) = match_name(&path,depth) {
-        t.into()
-    } else {
-        "()".to_string()
+fn remove_duplicates(s: &str) -> String {
+    let mut items: Vec<syn::Item> = syn::parse_file(s).unwrap().items;
+
+    let mut unique_items: Vec<syn::Item> = Vec::new();
+    let mut seen = std::collections::HashSet::<String>::new();
+
+    for item in items {
+        match &item {
+            Item::Struct(item_struct) => {
+                if seen.insert(item_struct.ident.to_string()) {
+                    unique_items.push(item);
+                }
+            }
+            Item::Enum(item_enum) => {
+                println!("{:?}",item_enum.ident.to_string());
+                if seen.insert(item_enum.ident.to_string()) {
+                    unique_items.push(item);
+                }
+            }
+            _ => {}
+        }
     }
+
+    println!("{:?}",seen);
+
+    let mut output = String::new();
+    for item in &unique_items {
+        output.push_str(&format!("{}\n", item.to_token_stream().to_string()));
+    }
+    output
 }
 
-fn match_name(type_path: &TypePath, depth: usize) -> Option<String> {
+// #[proc_macro]
+fn arguments(path: TypePath) -> (String,Option<String>) {
+    let depth: usize = 0;
+    let mut enumstruct = String::new();
+    let (s,t) = match_name(&path,depth,enumstruct);
+    (s.unwrap_or_else(|| "".to_string()),t)
+}
+
+fn match_name(type_path: &TypePath, depth: usize, mut enumstruct: String) -> (Option<String>,Option<String>) {
     let depth = depth + 1;
+    println!("{}", type_path.clone().path.segments.first().unwrap().ident.to_string());
     match type_path.clone().path.segments.first().unwrap().ident.to_string().as_str() {
         "u8" => {
-            Some(format!("u8"))
+            (Some(format!("u8")),None)
         }
         "u16" => {
-            Some(format!("u16"))
+            (Some(format!("u16")),None)
         }
         "u32" => {
-            Some(format!("u32"))
+            (Some(format!("u32")),None)
         }
         "u64" => {
-            Some(format!("u64"))
+            (Some(format!("u64")),None)
         }
         "i8" => {
-            Some(format!("i8"))
+            (Some(format!("i8")),None)
         }
         "i16" => {
-            Some(format!("i16"))
+            (Some(format!("i16")),None)
         }
         "i32" => {
-            Some(format!("i32"))
+            (Some(format!("i32")),None)
         }
         "i64" => {
-            Some(format!("i64"))
+            (Some(format!("i64")),None)
         }
         "f32" => {
-            Some(format!("f32"))
+            (Some(format!("f32")),None)
         }
         "f64" => {
-            Some(format!("f64"))
+            (Some(format!("f64")),None)
         }
         "String" => {
-            Some(format!("String"))
+            (Some(format!("String")),None)
         }
         "bool" => {
-            Some(format!("bool"))
+            (Some(format!("bool")),None)
         }
         "Vec" => {
-            let arg = match &type_path.clone().path.segments.first().unwrap().arguments{
+            let (arg,typ) = match &type_path.clone().path.segments.first().unwrap().arguments{
                 PathArguments::AngleBracketed(a) => match a.args.first().unwrap() {
                     GenericArgument::Type(t) => match t {
-                        Type::Path(f) => match_name(f,depth),
+                        Type::Path(f) => match_name(f,depth,enumstruct.clone()),
                         _ => todo!(),
                     },
                     _ => todo!(),                       
                 }                        
                 _ => todo!(),
             };
-            Some(format!("Vec<{}>",arg.unwrap()))
+            enumstruct.push_str(&typ.unwrap_or_else(|| "".to_string()));
+            (Some(format!("Vec<{}>",arg.unwrap())),Some(enumstruct))
         }
         "Option" => {
-            let arg = match &type_path.clone().path.segments.first().unwrap().arguments{
+            let (arg,typ) = match &type_path.clone().path.segments.first().unwrap().arguments{
                 PathArguments::AngleBracketed(a) => match a.args.first().unwrap() {
                     GenericArgument::Type(t) => match t {
-                        Type::Path(f) => match_name(f,depth),
+                        Type::Path(f) => match_name(f,depth,enumstruct.clone()),
                         _ => todo!(),
                     },
                     _ => todo!(),                       
                 }                        
                 _ => todo!(),
             };
-            Some(format!("Option<{}>",arg.unwrap()))
+            enumstruct.push_str(&typ.unwrap_or_else(|| "".to_string()));
+            (Some(format!("Option<{}>",arg.unwrap())),Some(enumstruct))
         }
         id => {
             let id: TokenStream2 = id.parse().unwrap();
-            handle_ident(id,depth)
+            handle_ident(id,depth,enumstruct.clone())
         }
     }   
 }
@@ -306,50 +373,63 @@ fn read_from_git_dependency(package_name: Option<String>, ident: &syn::Ident) ->
     None
 }
 
-fn handle_ident(ident: TokenStream2,depth: usize) -> Option<String> {
+fn handle_ident(ident: TokenStream2,depth: usize, enumstruct: String) -> (Option<String>,Option<String>) {    
     find_struct_or_enum_definition(&parse2::<Ident>(ident.clone()).unwrap()).map(|item| {
         match item {
             Item::Struct(item_struct) => {
-                handle_struct(item_struct,depth)                
+                handle_struct(item_struct,depth,enumstruct)                
             },
             Item::Enum(item_enum) => {
-                handle_enum(item_enum,depth)
+                handle_enum(item_enum,depth,enumstruct)
             },
             _ => {
-                None
+                (None,None)
             }
         }
     }).unwrap()
 }
 
-fn handle_struct_fields(item_struct: ItemStruct, depth: usize) -> String {
+fn handle_struct_fields(item_struct: ItemStruct, depth: usize, mut enumstruct: String) -> (String,Option<String>) {
     let mut field_streams = String::new();
     for field in item_struct.fields {
         let field_name = field.clone().ident.unwrap();
-        let field_type = match field.clone().ty {
+        let (field_type,field_enumstruct) = match field.clone().ty {
             syn::Type::Path(path) => {
-                match_name(&path,depth-1)
+                match_name(&path,depth-1,enumstruct.clone())
             },
-            _ => Some("".to_string()),
+            _ => (Some("".to_string()),Some("".to_string())),
         };
-        field_streams.push_str(format!{"{}{}: {},\n","\t".repeat(depth),field_name,field_type.unwrap()}.as_str());
-    }
-    field_streams
+        field_streams.push_str(format!{"\t{}: {},\n",field_name,field_type.unwrap()}.as_str());
+        enumstruct.push_str(&format!("{}",field_enumstruct.unwrap_or("".to_string())));
+    }    
+    (field_streams,Some(enumstruct))
 }
 
-fn handle_struct(item_struct: ItemStruct, depth: usize) -> Option<String> {
+fn handle_struct(item_struct: ItemStruct, depth: usize, mut enumstruct: String) -> (Option<String>,Option<String>) {
     let struct_name = item_struct.clone().ident;
-    let struct_fields = handle_struct_fields(item_struct.clone(),depth+1);
-    Some(format!("{} {{\n{}{}}}",struct_name,struct_fields,"\t".repeat(depth)))
+    let (struct_fields, fields_enumstruct) = handle_struct_fields(item_struct.clone(),depth+1,enumstruct.clone());
+    enumstruct.push_str(&format!("struct {} {{\n{}\n}}\n",
+        struct_name,
+        struct_fields,
+    ));
+    enumstruct.push_str(&format!("{}",fields_enumstruct.unwrap_or("".to_string())));
+    (Some(format!("{}",struct_name)),Some(enumstruct))
+    // Some(format!("{} {{\n{}{}}}",struct_name,struct_fields,"\t".repeat(depth)))
 }
 
-fn handle_enum(item_enum: ItemEnum, depth: usize) -> Option<String> {
+fn handle_enum(item_enum: ItemEnum, depth: usize, mut enumstruct: String) -> (Option<String>,Option<String>) {
     let enum_name = item_enum.clone().ident;
     let enum_variants = item_enum.clone().variants;
-    Some(format!("{} {{\n{}\n{}}}",
+    enumstruct.push_str(&format!("enum {} {{\n{}\n}}",
         enum_name,
         enum_variants.into_iter().map(|variant| {
-            format!("{}{},","\t".repeat(depth+1),variant.ident)}).collect::<Vec<String>>().join("\n"),
-        "\t".repeat(depth),
-    ))
+            format!("\t{},",variant.ident)}).collect::<Vec<String>>().join("\n"),       
+    ));
+    (Some(format!("{}",enum_name)),Some(enumstruct))
+    // Some(format!("{} {{\n{}\n{}}}",
+    //     enum_name,
+    //     enum_variants.into_iter().map(|variant| {
+    //         format!("{}{},","\t".repeat(depth+1),variant.ident)}).collect::<Vec<String>>().join("\n"),
+    //     "\t".repeat(depth),
+    // ))
 }
